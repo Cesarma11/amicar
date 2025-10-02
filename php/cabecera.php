285053dd2f8e38c5560830a64d06dfd5c5b85653
Cesarma11 <144679986+Cesarma11@users.noreply.github.com>
2025-10-02 17:12:53 +0200

    Add files via upload

---
+++ php/cabecera.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Autoescuela Amicar</title>
+    <link rel="icon" href="./image/L.ico" />
+    <link rel="stylesheet" href="./style.css" />
+  </head>
+  <body>
+    <!-- Header -->
+    <header>
+      <a href="./index.html"><img src="./image/logo.png" alt="Descripción de la imagen" width="180px"/></a>
+      <!-- <h1>Autoescuela am¡car</h1>-->
+      <nav>
+        <a href="index.html">Inicio</a>
+        <a href="nosotros.php">Quienes Somos</a>
+
+        <div class="dropdown">
+          <a class="dropbtn">Permisos</a>
+          <div class="dropdown-content">
+            <a href="permisoAutobus.php"><img src="./image/autobus.png" alt="imagenAutobus"  width="20" height="20" />Autobus</a>
+            <a href="#"><img src="./image/camion.png"  alt="imagenCamion"   width="20" height="20" />Camión</a>
+            <a href="#"><img src="./image/coche.png"   alt="imagenCoche"    width="20" height="20" />Coche</a>
+            <a href="#"><img src="./image/moto.png"    alt="imagenMoto"     width="20" height="20" />Moto</a>
+          </div>
+        </div>
+
+        <a href="#precios">Precios</a>
+        <div class="dropdown">
+          <a class="dropbtn">Centro de Formación</a>
+          <div class="dropdown-content">
+            <a href="#">Online</a>
+            <a href="#">Presencial</a>
+          </div>
+        </div>
+        <div class="dropdown">
+          <a class="dropbtn">Enlaces deInteres</a>
+          <div class="dropdown-content">
+            <a href="#">Revista DGT</a>
+            <a href="#">Señales de trafico</a>
+            <a href="#">Notas de examen</a>
+            <a href="#">Incidencias Trafico</a>
+          </div>
+        </div>
+        <a href="#AreaPersonal">Area Personal</a>
+        <a href="#contacto">Contacto</a>
+        <img src="./image/l practicas.png" alt="" style="width:5%; margin-left: 300px; margin-top:10px; " />
+      </nav>
+    </header>
